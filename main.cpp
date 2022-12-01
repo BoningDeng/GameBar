813955103da8551865c11b7a13e52d86419999db
BoningDeng <bdeng@ufl.edu>
2022-11-30 23:24:27 -0500

    read file works

--- main.cpp
+++ main.cpp
@@ -7,24 +7,34 @@ using namespace std;
 
 int main()
 {
-	string name = "The Legend of Zelda : Ocarina of Time";
-	string platform = "Nintendo 64";
-	string release_date = "23-Nov-98";
-	string summary = "As a young boy, Link is tricked by Ganondorf, the King of the Gerudo Thieves. The evil human uses Link to gain access to the Sacred Realm, where he places his tainted hands on Triforce and transforms the beautiful Hyrulean landscape into a barren wasteland. Link is determined to fix the problems he helped to create, so with the help of Rauru he travels through time gathering the powers of the Seven Sages.";
-	int meta_score = 99;
-	float user_review = 9.1;
-	Game temp(name, platform, release_date, summary, meta_score, user_review);
-	std::cout << name << std::endl;
-	std::cout << platform << std::endl;
-	std::cout << release_date << std::endl;
-	std::cout << summary << std::endl;
-	std::cout << meta_score << std::endl;
-	std::cout << user_review << std::endl;
+	string name;
+	string platform;
+	string release_date;
+	string summary;
+	string tempMeta_score;
+	string tempUser_review;
+	int meta_score;
+	float user_review;
 
 	vector<Game> Games;
 
 	fstream file;
 	file.open("all_games.csv", ios::in);
+	while (!file.eof())
+	{
+		getline(file, name, ',');
+		getline(file, platform, ',');
+		getline(file, release_date, ',');
+		getline(file, tempMeta_score, ',');
+		getline(file, tempUser_review, ',');
+		meta_score = stoi(tempMeta_score);
+		user_review = stof(tempUser_review);
+		getline(file, summary);
 
+		Game temp(name, platform, release_date, summary, meta_score, user_review);
+		Games.push_back(temp);
+	}
+
+	cout << Games.size() << endl;
 	return 0;
 }
