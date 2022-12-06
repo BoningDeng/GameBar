2873587b4c79216afb3776fb3b60ab65a06f3f7e
ZafMendes <119459998+ZafMendes@users.noreply.github.com>
2022-12-05 19:39:59 -0500

    Update mergeSort.h

--- mergeSort.h
+++ mergeSort.h
@@ -32,7 +32,55 @@ void merge(vector<Game*> arr, int left, int middle, int right){
  
     // Merge both temp arrays into a single 
     while (leftIndex < leftSub && rightIndex < rightSub) {
-        if (L[leftIndex] <= R[rightIndex]) {
+        if (L[leftIndex]->getUser_review() <= R[rightIndex]->getUser_review()) {
+            arr[merged] = L[leftIndex];
+            leftIndex++;
+        }
+        else {
+            arr[merged] = R[rightIndex];
+            rightIndex++;
+        }
+        merged++;
+    }
+                    
+    // Copy the remaining elements of left and right
+    while (leftIndex < leftSub) {
+        arr[merged] = L[leftIndex];
+        leftIndex++;
+        merged++;
+    }
+    while (rightIndex < rightSub) {
+        arr[merged] = R[rightIndex];
+        rightIndex++;
+        merged++;
+    }
+    
+}
+
+void merge2(vector<Game*> arr, int left, int middle, int right){
+    int const leftSub = middle - left + 1;
+    int const rightSub = right - middle;
+ 
+    // Create temp arrays for left and right 
+    vector<Game*> L, R;
+    
+    L.resize(leftSub);
+    R.resize(rightSub);
+ 
+    // Copy data from our game Array into the temporary arrays.
+    for (int i = 0; i < leftSub; i++)
+        L[i] = arr[left + i];
+    for (int i = 0; i < rightSub; i++)
+        R[i] = arr[middle + 1 + i];
+ 
+    //Declare indexes for the temporary arrays, those will be updated while merging
+    int leftIndex = 0;
+    int rightIndex = 0;
+    int merged = 0;
+ 
+    // Merge both temp arrays into a single 
+    while (leftIndex < leftSub && rightIndex < rightSub) {
+        if (L[leftIndex]->getMeta_score() <= R[rightIndex]->getMeta_score()) {
             arr[merged] = L[leftIndex];
             leftIndex++;
         }
@@ -60,17 +108,25 @@ void merge(vector<Game*> arr, int left, int middle, int right){
 
 //MergeSort main function
 void mergeSort(vector<Game*> arr, int start, int end){ 
-  //Return if left element is bigger than rightmost
-    if (start >= end)
-        return; 
+    if (input == 1){
+        if (start >= end)
+            return; 
  
-    int middle = start + (end - start) / 2;
+        int middle = start + (end - start) / 2;
   
-    //Recursively call mergeSort for left array (left to middle) and right array (middle to right)
-    mergeSort(arr, start, middle);
-    mergeSort(arr, middle + 1, end);
-  
-    
-    merge(arr, start, middle, end);
+        //Recursively call mergeSort for left array (left to middle) and right array (middle to right)
+        mergeSort(arr, start, middle);
+        mergeSort(arr, middle + 1, end);
+        merge(arr, start, middle, end);
+        
+    } else if (input == 2) {
+        if (start >= end)
+            return; 
+ 
+        int middle = start + (end - start) / 2;
+        mergeSort(arr, start, middle);
+        mergeSort(arr, middle + 1, end);
+        merge2(arr, start, middle, end);
+    }   
   
 }  
